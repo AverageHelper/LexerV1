5e28bc32fb7834693e48951de5d2715165ebd1e4
SparrowBlaze <jnrobinson016@gmail.com>
2019-10-08 17:49:37 -0600

    Commit DatalogProgram header

---
+++ LexerV1/DatalogProgram.h
@@ -0,0 +1,42 @@
+//
+//  DatalogProgram.h
+//  LexerV1
+//
+//  Created by James Robinson on 9/30/19.
+//
+
+#ifndef DatalogProgram_h
+#define DatalogProgram_h
+
+#include "Production.h"
+#include "Predicate.h"
+#include "Rule.h"
+
+class DatalogProgram: public Production {
+private:
+    std::string identifier;
+    std::vector<Predicate*> schemes;
+    std::vector<Predicate*> facts;
+    std::vector<Rule*>      rules;
+    std::vector<Predicate*> queries;
+    
+public:
+    DatalogProgram(std::string identifier = "");
+    ~DatalogProgram();
+    
+    void setIdentifier(std::string identifier);
+    int addScheme(Predicate* scheme);
+    int addFact(Predicate* fact);
+    int addRule(Rule* rule);
+    int addQuery(Predicate* query);
+    void setSchemes(std::vector<Predicate*>& schemes);
+    void setFacts(std::vector<Predicate*>& facts);
+    void setRules(std::vector<Rule*>& rules);
+    void setQueries(std::vector<Predicate*>& queries);
+    
+    std::string getIdentifier();
+    std::string toString() override;
+    const std::vector<std::string> getDomain();
+};
+
+#endif /* DatalogProgram_h */
